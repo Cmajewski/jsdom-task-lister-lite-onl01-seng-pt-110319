4e151cc8713c9da4185c2b354a1693a7d90a6ed4
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-16 13:08:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 
   // your code here
-  const taskForm=document.querySelector("#create-task-form");
-  const tasks=document.querySelector("#tasks")
- 
-  taskForm.addEventListener("submit", function(event){
-    event.preventDefault()
-    const newTask=document.querySelector("#new-task-description").value
-    
-    tasks.innerHTML+=`
-    <li>${newTask}
-    <button data-action="delete"> X</button>
-    </li>`
-    // const taskCreate=document.createElement("li")
-  
-    // taskCreate.innerText=newTask
-    // tasks.appendChild(taskCreate)
-    taskForm.reset()
+let submit=document.getElementByID("submit")
+let search=document.getElementByID("new-task-description").value
 
-  });
+submit.addEventListener("click",function(e){
+  event.preventDefault();
 
-  tasks.addEventListener("click", function(event){
-      if (event.target.dataset.action==="delete"){
-        event.target.parentElement.remove()
-      }
-  });
+})
